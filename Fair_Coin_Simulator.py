d8124c7b63fb87295946c7ee1a83f266643a1048
Orpheus <73367612+Orphyeus@users.noreply.github.com>
2024-02-08 23:55:28 +0300

    Update Fair_Coin_Simulator.py

--- Fair_Coin_Simulator.py
+++ Fair_Coin_Simulator.py
@@ -2,7 +2,7 @@ from random import randrange
 
 for test in [100,1000,10000,100000, 1000000, 10000000]:
     heads = tails = 0
-    for i in range (experiment) :
+    for i in range (test) :
         if randrange (2) == 0: heads = heads + 1
         else: tails = tails + 1
     print("Test:", test)
